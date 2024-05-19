e577683c2b152043751ef1f7775919586134cbe7
Dehveyn <evseenko-daniil@yandex.ru>
2024-05-19 23:29:21 +0300

    Revert "почистил playerInput"
    This reverts commit 3852e70855cdcec4de134d908a7c68f83e5c96a8.

--- Assets/Scripts/Components/Input/PlayerInputData.cs
+++ Assets/Scripts/Components/Input/PlayerInputData.cs
@@ -12,6 +12,15 @@ namespace Components {
         public Vector2 lookInput;
         public bool jumpTriggered;
         public float sprintValue;
+        
+        
+        public InputAction moveAction;
+        
+        public InputAction lookAction;
+        [HideInInspector]
+        public InputAction jumpAction;
+        [HideInInspector]
+        public InputAction sprintAction;
     }
     
     class InputPlayerTemplate : ComponentTemplate<PlayerInputData> { }
